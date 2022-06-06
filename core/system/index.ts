3604247a301ee0fa7a286c2cc43a6dacdc80f11b
higuchi.fumito <higuchi.fumito@mail.canon>
2022-06-06 23:40:10 +0900

    aa

--- core/system/index.ts
+++ core/system/index.ts
@@ -4,6 +4,7 @@ export { default as CreateFn } from "@csea/core/system/create"
 export { default as UpdateFn } from "@csea/core/system/update"
 export { default as FindFn } from "@csea/core/system/find"
 export { default as FilterFn } from "@csea/core/system/filter"
+export { default as DeleteFn } from "@csea/core/system/delete"
 
 export type System = {
   id: string
