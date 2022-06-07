ea93cbb1dac0bbd671d4998c3c4f8f7743751957
higuchi.fumito <higuchi.fumito@mail.canon>
2022-06-07 17:42:59 +0900

    add web

---
+++ web/App.tsx
@@ -0,0 +1,10 @@
+import React, { Suspense, lazy } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      hello
+    </Router>
+  );
+} 
