fac570b6546a204f471c427a838aea17f33c180d
higuchi.fumito <higuchi.fumito@mail.canon>
2022-06-10 21:24:47 +0900

    fix

---
+++ api/api.spec.ts
@@ -0,0 +1,55 @@
+import { RootApi } from ".";
+import { ErrorKind } from "@csea/core";
+import { v4 as uuid } from "uuid";
+
+const api = RootApi();
+api.setUrl(`http://srv/${process.env.PREFIX}`);
+
+
+  describe("system", () => {
+  const id = uuid();
+  test("create", async () => {
+    const res = await api.system.create({
+      id: id,
+      name: uuid(),
+      code: uuid(),
+    });
+    if (res instanceof Error) {
+
+    }
+  });
+  test("delete", async () => {
+    const res = await api.system.delete({
+      id: id,
+    });
+    if (res instanceof Error) {
+      expect(res.message).toBe(ErrorKind.SystemNotFound);
+    }
+  });
+  test("update", async () => {
+    const res = await api.system.update({
+      id: uuid(),
+      name: uuid(),
+      code: uuid(),
+    });
+    if (res instanceof Error) {
+      expect(res.message).toBe(ErrorKind.SystemNotFound);
+    }
+  });
+  test("filter", async () => {
+    const res = await api.system.filter({
+    });
+    if (res instanceof Error) {
+      throw res;
+    }
+  });
+  test("find", async () => {
+    const res = await api.system.find({
+      id: uuid(),
+    });
+    if (res instanceof Error) {
+      expect(res.message).toBe(ErrorKind.SystemNotFound);
+    }
+  });
+});
+
