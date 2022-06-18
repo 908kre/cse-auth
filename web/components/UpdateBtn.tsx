ab2f6ea4a5b64ad2d3e739f18bca1a09472e10e8
yao <yao.ntno@gmail.com>
2022-06-18 16:12:00 +0000

    fix build

--- web/components/UpdateBtn.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function UpdateBtn(props) {
-  return (
-    <div className="button is-small" {...props}>
-      <i className="fas fa-edit"></i>
-    </div>
-  );
-}
