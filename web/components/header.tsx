2b3ac99e5754d2699042ada0cc7a2d7198e26b5b
iritani.shota <iritani.shota@mail.canon>
2022-06-30 20:34:26 +0900

    fix: logout layout

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -6,19 +6,37 @@ export const Header = (props: {
   onLogout?: VoidFunction
 }) => {
   const navigate = useNavigate()
+  const [isActive, setIsActive] = React.useState(false);
+  const toggle = () => {
+    setIsActive(!isActive);
+  };
+
   const { mutate } = useSWRConfig();
   return (
     <nav className="navbar is-dark" aria-label="main navigation">
       <div className="navbar-brand">
         <a className="navbar-item">SmaFa-AUTH</a>
-        <span></span>
-        <span></span>
-        <span></span>
+        <div
+            className="navbar-burger burger"
+            data-target="navMenubd-example"
+            onClick={() => toggle()}
+          >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-      <div className="navbar-brand">
-        <a className="navbar-item" onClick={() => navigate("/system")}>システム</a>
-        <a className="navbar-item" onClick={() => navigate("/role")}>ロール</a>
-        <a className="navbar-item" onClick={() => props.onLogout?.()}>ログアウト</a>
+      <div 
+          className={isActive ? "navbar-menu is-active" : "navbar-menu"}
+          id="navMenubd-example"
+        >
+        <div className="navbar-start">
+          <a className="navbar-item" onClick={() => navigate("/system")}>システム</a>
+          <a className="navbar-item" onClick={() => navigate("/role")}>ロール</a>
+        </div>
+        <div className="navbar-end">
+          <a className="navbar-item" onClick={() => props.onLogout?.()}>ログアウト</a>
+        </div>
       </div>
     </nav>
   );
