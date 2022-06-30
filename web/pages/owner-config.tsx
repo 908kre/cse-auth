559107e6539b74e6549cfead65562f5e0c5b4190
908kre <ce51046.higuchi@gmail.com>
2022-06-30 10:37:15 +0000

    fix

--- web/pages/owner-config.tsx
+++ web/pages/owner-config.tsx
@@ -14,39 +14,26 @@ import { useForm } from "react-hook-form";
 const api = Api();
 const toast = useToast();
 export const OwnerConfigPage = () => {
-//   const {
-//         handleSubmit,
-//         formState: { errors },
-//       } = useForm<Payload>({
-//         defaultValues: {
-//           id: props.system?.id,
-//           name: props.system?.name,
-//         },
-//       });
   const { mutate } = useSWRConfig();
   const navigate = useNavigate();
-  //let { id } = useParams();
   const { data: owners } = useSWR("/owner", () => api.user.filter({}));
   if (owners === undefined || owners instanceof Error) {
-    console.log("user " + owners)
     return <Loading />;
   }
-  const clickHandle = async ({value}) => {
-    console.log("QQQ")
-    const err = await api.user.setAdmin({ id: value })
-    console.log(err)
-    if(err instanceof Error) {return toast.error(err.message)}
-      mutate("/owner")
-      toast.info('成功しました')
-  }
+
   const endStyle: React.CSSProperties = { display: "flex", justifyContent: "flex-end", flexWrap: "nowrap" };
   const columns= [
-    { key: "userId", name: "ID", width: "90%"},
+    { key: "id", name: "ID", width: "90%"},
     { key: "action", name: "操作",
       formatter(column) {
         return (
           <div className="buttons" style={endStyle}>
-            <DeleteBtn onClick={() => clickHandle(column.row.userId)} />
+            <DeleteBtn onClick={async() => {
+              const err = await api.user.setAdmin({ id: column.row.id })
+              if(err instanceof Error) {return toast.error(err.message)}
+              mutate("/owner")
+              toast.info('成功しました')
+            }} />
           </div>
         )
       },
@@ -58,37 +45,24 @@ export const OwnerConfigPage = () => {
       <label className="label is-medium">
         オーナー
       </label>
-        <div className="p-1" style={{ borderTop: '0.5px solid #d3d3d3', width: "100%" }}/>
-        <Form
-          placeholder={"ユーザーID"}
-          onSubmit={clickHandle()}
-        />
-        <DataGrid
-          columns={columns}
-          rows={owners}
-          defaultColumnOptions={{
+      <div className="p-1" style={{ borderTop: '0.5px solid #d3d3d3', width: "100%" }}/>
+      <Form
+        placeholder={"ユーザーID"}
+        onSubmit={async ({value}) => {
+          const err = await api.user.setAdmin({ id: value })
+          if(err instanceof Error) {return toast.error(err.message)}
+          mutate("/owner")
+          toast.info('成功しました')
+        }}
+      />
+      <DataGrid
+        columns={columns}
+        rows={owners}
+        defaultColumnOptions={{
           resizable: true,
         }}
-        />
-
-      </div>
-)
+      />
+    </div>
+  )
 }
-    
 
-
-
-
- {/* <UserTable
-  rows={users}
-  onDelete={async (value) => {
-    const err = await api.roleUser.delete({userId:value, roleId:role.id ?? ""})
-    if(err instanceof Error) {return toast.error(err.message)}
-    mutate("/role/user")
-    toast.info('成功しました')
-}}
-/> 
-  
-</div>
-  );
-}; */}
