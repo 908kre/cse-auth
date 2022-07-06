81af189175107c156fa09bc5ff475e5361b92810
higuchi.fumito <higuchi.fumito@mail.canon>
2022-07-06 20:35:23 +0900

    git

--- server/store/ldap.spec.ts
+++ server/store/ldap.spec.ts
@@ -1,6 +1,6 @@
 import { Store } from '@csea/server/store/ldap'
 
-describe("gcip", () => {
+describe("ldap", () => {
   const store = Store()
   test("find-true", async () => {
     const rows = await store.find({id: "AAA111633", password: "cipdev"})
